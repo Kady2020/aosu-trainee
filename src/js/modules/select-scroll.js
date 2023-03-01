d9bbf103b9aa15eeaf44bee8644f83670dd00761
Kady <74194999+Kady2020@users.noreply.github.com>
2023-03-01 14:32:40 +0300

    added fixed select form

--- src/js/modules/select-scroll.js
+++ src/js/modules/select-scroll.js
@@ -6,3 +6,12 @@ $(window).scroll(function () {
     $('.form').removeClass('scroll-form');
   }
 });
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    $('.select-content').addClass('select-content_scroll');
+  }
+  else {
+    $('.select-content').removeClass('select-content_scroll');
+  }
+});
